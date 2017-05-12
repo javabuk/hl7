a3d45d055f19a136fbb81122f2322fbb53f3a26b
javabuk <joprieto@gmail.com>
2017-05-12 20:59:14 +0200

    Cambios hechos en la rama de prueba, después del mergeo.

--- ParserHL7/src/main/java/org/hl7/validaciones/Validaciones2_5.java
+++ ParserHL7/src/main/java/org/hl7/validaciones/Validaciones2_5.java
@@ -27,9 +27,15 @@ public class Validaciones2_5 extends DefaultValidationBuilder {
 		// forVersion(Version.V25).message("ADT", "*").terser("MSH-3-1",
 		// isEqualIgnoreCase("MEDIKOSTA"));
 		// Validamos que el campo emisor este comprendido en una lista
+		/*
+		 * List<String> listaEmisoresValidos = new ArrayList<String>();
+		 * listaEmisoresValidos.add("BRANCH");
+		 * listaEmisoresValidos.add("INFO33");
+		 */
 		List<String> listaEmisoresValidos = new ArrayList<String>();
-		listaEmisoresValidos.add("BRANCH");
-		listaEmisoresValidos.add("INFO33");
+		listaEmisoresValidos.add("JORGE");
+		listaEmisoresValidos.add("BARAKALDO");
+
 		forVersion(Version.V25).message("ADT", "*").terser("MSH-3-1", in(listaEmisoresValidos));
 
 		// Realizamos varias validaciones sobre un mismo campo
